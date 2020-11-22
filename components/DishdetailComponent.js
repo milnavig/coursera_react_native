1e539e0ba0a75649c20b8c829ed1f2ff1129dc93
milnavig <milnavig@gmail.com>
2020-11-22 15:55:37 +0200

    Third assignment

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -35,6 +35,13 @@ function RenderDish(props) {
         else
             return false;
     }
+    
+    const recognizeComment = ({ moveX, moveY, dx, dy }) => {
+        if ( dx > 200 )
+            return true;
+        else
+            return false;
+    }
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: (e, gestureState) => {
@@ -43,7 +50,7 @@ function RenderDish(props) {
         onPanResponderGrant: () => {this.view.rubberBand(1000).then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));},
         onPanResponderEnd: (e, gestureState) => {
             console.log("pan responder end", gestureState);
-            if (recognizeDrag(gestureState))
+            if (recognizeDrag(gestureState)) {
                 Alert.alert(
                     'Add Favorite',
                     'Are you sure you wish to add ' + dish.name + ' to favorite?',
@@ -53,7 +60,10 @@ function RenderDish(props) {
                     ],
                     { cancelable: false }
                 );
-
+            } else if (recognizeComment(gestureState)) {
+                props.open();
+            }
+                
             return true;
         }
     })
